104df6551eb0177c80466b57974a89524ccb6b32
jpwenzel <jeanpierre.wenzel@gmail.com>
2011-08-18 20:58:13 +0200

    Updated conversion script: temporary file generation, comments.

--- audio2mp3.sh
+++ audio2mp3.sh
@@ -4,7 +4,7 @@
 # A simple MPEG-4 to MPEG-3 (audio) conversion wrapper using faad and lame.
 # -----------------------------------------------------------------------------
 #
-# Based on the m4a2mp3 and FLAC conversion shell scripts from
+# Inspired by the m4a2mp3 and FLAC conversion shell scripts from
 # http://wiki.linuxquestions.org/wiki/.m4a_to_.mp3 and 
 # https://wiki.archlinux.org/index.php/Convert_Flac_to_Mp3
 #
@@ -17,7 +17,7 @@
 # Copyright (C) 2011 by Jean Pierre Wenzel <jpwenzel@gmx.net>.
 # -----------------------------------------------------------------------------
 
-for DEPENDENCY in faad lame; do
+for DEPENDENCY in lame; do
 	hash "${DEPENDENCY}" 2>&- || {
 		echo >&2 "${DEPENDENCY} is required but not installed. Aborting."; 
 		exit 1;
@@ -25,17 +25,15 @@ for DEPENDENCY in faad lame; do
 done
 
 for INPUT_FILENAME in "$@"; do
-	BASE_FILENAME=$(echo ${INPUT_FILENAME} | sed -n 's/\.[Mm]4[Aa]$//p')
+	BASE_FILENAME=${INPUT_FILENAME%.*}
 	[ -z "${BASE_FILENAME}" ] && continue
 
 	OUTPUT_FILENAME="${BASE_FILENAME}.mp3"
-	TMP_INFO="${BASE_FILENAME}.info"
- 	TMP_OPTS="${BASE_FILENAME}.opts"
- 	TMP_WAV="${BASE_FILENAME}.wav"
- 	
- 	rm -f "${TMP_INFO}" "${TMP_OPTS}" "${TMP_WAV}"
+	TMP_INFO=$(mktemp "${BASE_FILENAME}-info")
+ 	TMP_OPTIONS=$(mktemp "${BASE_FILENAME}-options")
+ 	TMP_WAVE=$(mktemp "${BASE_FILENAME}-wave")
 		
-	# Extract info (artist, title, etc.) from source m4a file.
+	# Extract info (artist, title, etc.) from source file.
 	faad -i "${INPUT_FILENAME}" 2> "${TMP_INFO}"
 	
 	cat "${TMP_INFO}" | sed -n '
@@ -46,13 +44,13 @@ for INPUT_FILENAME in "$@"; do
 		s/^album: \(.*\)$/--tl "\1"/p
 		s/^track: \(.*\)$/--tn "\1"/p
 		s/^date: \([12][0-9][0-9][0-9]\)$/--ty "\1"/p
-		' > "${TMP_OPTS}"
+		' > "${TMP_OPTIONS}"
 	
-	rm "${TMP_INFO}"
+	# Convert from source to wave format
+	faad "${INPUT_FILENAME}" -o "${TMP_WAVE}"
 	
-	# Convert the m4a file
-	faad "${INPUT_FILENAME}" -o "${TMP_WAV}"
-	xargs lame -V2 --vbr-new "${TMP_WAV}" "${OUTPUT_FILENAME}" < "${TMP_OPTS}"
+	# Compress using lame
+	xargs lame -V2 --vbr-new "${TMP_WAVE}" "${OUTPUT_FILENAME}" < "${TMP_OPTIONS}"
 	
-	rm "${TMP_OPTS}" "${TMP_WAV}"
+	rm -f "${TMP_INFO}" "${TMP_OPTIONS}" "${TMP_WAVE}"
 done
