8d89923cde74d810ada7f51acd8e480059ef8ada
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2018-07-30 23:05:53 +0200

    Using AtomicParsley for MP3 tags

--- audio2mp3.sh
+++ audio2mp3.sh
@@ -1,56 +1,59 @@
-#!/bin/sh
+#!/bin/bash -e
 #
 # -----------------------------------------------------------------------------
 # A simple MPEG-4 to MPEG-3 (audio) conversion wrapper using faad and lame.
 # -----------------------------------------------------------------------------
 #
 # Inspired by the m4a2mp3 and FLAC conversion shell scripts from
-# http://wiki.linuxquestions.org/wiki/.m4a_to_.mp3 and 
+# http://wiki.linuxquestions.org/wiki/.m4a_to_.mp3 and
 # https://wiki.archlinux.org/index.php/Convert_Flac_to_Mp3
 #
 # Dependencies:
-#   * lame:  http://lame.sourceforge.net/
-#   * faad2: http://www.audiocoding.com/ (optional)
-#   * flac:  http://flac.sourceforge.net/ (optional)
+#   * lame:          http://lame.sourceforge.net/
+#   * faad2:         http://www.audiocoding.com/
+#   * AtomicParsley: 
 #
 # -----------------------------------------------------------------------------
-# Copyright (C) 2011 by Jean Pierre Wenzel <jpwenzel@gmx.net>.
+# Copyright (C) 2011-2018 Jean Pierre Wenzel <jpwenzel@gmx.net>.
 # -----------------------------------------------------------------------------
 
-for DEPENDENCY in lame; do
+for DEPENDENCY in lame faad AtomicParsley; do
 	hash "${DEPENDENCY}" 2>&- || {
-		echo >&2 "${DEPENDENCY} is required but not installed. Aborting."; 
+		echo >&2 "${DEPENDENCY} is required but not installed. Aborting.";
 		exit 1;
 	}
 done
 
-for INPUT_FILENAME in "$@"; do
-	BASE_FILENAME=${INPUT_FILENAME%.*}
-	[ -z "${BASE_FILENAME}" ] && continue
+INPUT_FILENAME="$1"
+BASE_FILENAME="${INPUT_FILENAME%.*}"
+[ -z "${BASE_FILENAME}" ] && continue
 
-	OUTPUT_FILENAME="${BASE_FILENAME}.mp3"
-	TMP_INFO=$(mktemp "${BASE_FILENAME}-info")
- 	TMP_OPTIONS=$(mktemp "${BASE_FILENAME}-options")
- 	TMP_WAVE=$(mktemp "${BASE_FILENAME}-wave")
-		
-	# Extract info (artist, title, etc.) from source file.
-	faad -i "${INPUT_FILENAME}" 2> "${TMP_INFO}"
-	
-	cat "${TMP_INFO}" | sed -n '
-		s/[\"$]//g
-		s/[\`$]//g
-		s/^title: \(.*\)$/--tt "\1"/p
-		s/^artist: \(.*\)$/--ta "\1"/p
-		s/^album: \(.*\)$/--tl "\1"/p
-		s/^track: \(.*\)$/--tn "\1"/p
-		s/^date: \([12][0-9][0-9][0-9]\)$/--ty "\1"/p
-		' > "${TMP_OPTIONS}"
-	
-	# Convert from source to wave format
-	faad "${INPUT_FILENAME}" -o "${TMP_WAVE}"
-	
-	# Compress using lame
-	xargs lame -V2 --vbr-new "${TMP_WAVE}" "${OUTPUT_FILENAME}" < "${TMP_OPTIONS}"
-	
-	rm -f "${TMP_INFO}" "${TMP_OPTIONS}" "${TMP_WAVE}"
-done
+OUTPUT_FILENAME="${BASE_FILENAME}.mp3"
+TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/audio2mp3.XXXXXXXXX")
+TMP_WAVE="$TMP_DIR"/wave
+
+# Gather audio file meta information
+PREV_LC_ALL=$LC_ALL
+export LC_ALL=C
+MP3_TAG_TITLE=$(AtomicParsley "${INPUT_FILENAME}" -t | grep '©nam' | sed -e 's/^Atom "©nam" contains: //g')
+MP3_TAG_ARTIST=$(AtomicParsley "${INPUT_FILENAME}" -t | grep '©ART' | sed -e 's/^Atom "©ART" contains: //g')
+MP3_TAG_ALBUM=$(AtomicParsley "${INPUT_FILENAME}" -t | grep '©alb' | sed -e 's/^Atom "©alb" contains: //g')
+MP3_TAG_TRACK=$(AtomicParsley "${INPUT_FILENAME}" -t | grep 'trkn' | sed -e 's/^Atom "trkn" contains: //g' | egrep -o '^\d*')
+MP3_TAG_DATE=$(AtomicParsley "${INPUT_FILENAME}" -t | grep '©day' | sed -e 's/^Atom "©day" contains: //g')
+export LC_ALL=$PREV_LC_ALL
+
+LAME_ARGS=(\
+--tt "$MP3_TAG_TITLE" \
+--ta "$MP3_TAG_ARTIST" \
+--tl "$MP3_TAG_ALBUM" \
+--tn "$MP3_TAG_TRACK" \
+--ty "$MP3_TAG_DATE" \
+)
+
+# Convert from source to wave format
+faad "${INPUT_FILENAME}" -o "${TMP_WAVE}"
+
+# Compress using lame
+lame -v -V 2 -b 128 "${TMP_WAVE}" "${OUTPUT_FILENAME}" "${LAME_ARGS[@]}"
+
+rm "${TMP_WAVE}"
